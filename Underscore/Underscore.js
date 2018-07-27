01386d215977654c8dcd509e093c446eca9cd545
Tatiana <sychevatatyanav@gmail.com>
2018-07-27 20:46:16 +0700

    Underscore

--- Underscore/Underscore.js
+++ Underscore/Underscore.js
@@ -1,14 +1,46 @@
-var peoples = [{name: "Ivan", age: 20, lastName: "Ivanov"}, {name: "Petr", age: 45, lastName: "Petrov"},
-    {name: "Maria", age: 26, lastName: "Ivanova"}, {name: "Fedor", age: 54, lastName: "Fedorov"}, {
-        name: "Marsim",
-        age: 32, lastName: "Viktorov"
-    }, {name: "Tatiana", age: 35, lastName: "Viktorova"}, {name: "Pavel", age: 28, lastName: "Ivanov"},
-    {name: "Anna", age: 27, lastName: "Vladimirova"}, {name: "Aleksandr", age: 36, lastName: "Borisov"}, {
-        name: "Alla",
-        age: 24, lastName: "Nikolaeva"
-    }];
+var peoples = [{
+    name: "Ivan",
+    age: 20,
+    lastName: "Ivanov"
+}, {
+    name: "Petr",
+    age: 45,
+    lastName: "Petrov"
+}, {
+    name: "Maria",
+    age: 26,
+    lastName: "Ivanova"
+}, {
+    name: "Fedor",
+    age: 54,
+    lastName: "Fedorov"
+}, {
+    name: "Marsim",
+    age: 32,
+    lastName: "Viktorov"
+}, {
+    name: "Tatiana",
+    age: 35,
+    lastName: "Viktorova"
+}, {
+    name: "Pavel",
+    age: 28,
+    lastName: "Ivanov"
+}, {
+    name: "Anna",
+    age: 27,
+    lastName: "Vladimirova"
+}, {
+    name: "Aleksandr",
+    age: 36,
+    lastName: "Borisov"
+}, {
+    name: "Alla",
+    age: 24,
+    lastName: "Nikolaeva"
+}];
 
-var ages = _.pluck(peoples, 'age');
+var ages = _.pluck(peoples, "age");
 var averageAge = _.reduce(ages, function (memo, num) {
     return memo + num;
 }, 0) / ages.length;
@@ -27,7 +59,7 @@ console.log("Отсортированный список людей от 20 до
 console.log(sortedPeople);
 
 _.each(peoples, function (peoples) {
-    peoples["fullName"] = peoples.name + ' ' + peoples.lastName;
+    peoples.fullName = peoples.name + " "+ peoples.lastName;
 });
 
 console.log("список людей с полем fullName:");
